0b1729f0d040d767d17a8be4e5e6155d9fcaf6a9
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-25 19:31:55 -0700

    removed API key

--- assets/logic.js
+++ assets/logic.js
@@ -1,9 +1,7 @@
 
 
 var musixCall = fetch("https://api.musixmatch.com/ws/1.1/", {
-    apikey: '740dcd30745d6e3536b315d57ead722b',
 });
-
 console.log(musixCall)
 console.log("Connected to HTML")
 // Establishing API's from Spotify/SoundCloud and MusiX
