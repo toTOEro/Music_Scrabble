657d848ecb799c8c3f326188ec811e7af8549136
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-25 20:28:17 -0700

    setting up github actions

--- assets/logic.js
+++ assets/logic.js
@@ -1,7 +1,9 @@
 
-
-var musixCall = fetch("https://api.musixmatch.com/ws/1.1/", {
+var musixCall = fetch(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=cumulonimbus&page_size=3&page=1&s_track_rating=desc&apikey=${apiKEY}`, {
 });
+
+var apiKEY = "740dcd30745d6e3536b315d57ead722b"
+
 console.log(musixCall)
 console.log("Connected to HTML")
 // Establishing API's from Spotify/SoundCloud and MusiX
@@ -9,4 +11,3 @@ console.log("Connected to HTML")
 
 // Logic to determine song from words
 
-https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=cumulonimbus&page_size=3&page=1&s_track_rating=desc&apikey=
