4c0808a423f8de7d1302c562de5b494cfac9a407
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-25 18:49:48 -0700

    setting up API fetch

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,8 @@
 
 
-var musixCall = fetch("https://api.musixmatch.com/ws/1.1/")
+var musixCall = fetch("https://api.musixmatch.com/ws/1.1/", {
+    apikey: '740dcd30745d6e3536b315d57ead722b',
+});
 
 console.log(musixCall)
 console.log("Connected to HTML")
