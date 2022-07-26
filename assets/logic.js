6c6364f196e04d21ccf2dfa1b9f6535a7bfd74cd
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-25 19:45:16 -0700

    saving API url to pulll  songs

--- assets/logic.js
+++ assets/logic.js
@@ -7,7 +7,6 @@ console.log("Connected to HTML")
 // Establishing API's from Spotify/SoundCloud and MusiX
 
 
-
-
 // Logic to determine song from words
 
+https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=cumulonimbus&page_size=3&page=1&s_track_rating=desc&apikey=
