cf9e40892149513b8fbc44b5671eae351a91edbb
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-25 21:01:22 -0700

    setting up apiKEY as a variable

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,16 @@
 
-var apiKEY = ""
+var apiKEY = "740dcd30745d6e3536b315d57ead722b"
 
 var musixCall = fetch(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=cumulonimbus&page_size=3&page=1&s_track_rating=desc&apikey=${apiKEY}`, {
 });
 
 
 
-console.log(musixCall)
-console.log("Connected to HTML")
+// console.log(musixCall)
+// console.log("Connected to HTML")
 
-console.log(process.env.MUSIXMATCH_API)
+
+// console.log(process.env.MUSIXMATCH_API)
 
 // Establishing API's from Spotify/SoundCloud and MusiX
 
