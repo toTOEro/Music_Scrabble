8a65bbc5444369b63b4d989f89efd9703f10fbcf
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-25 20:43:54 -0700

    re-commit because of rebase error

--- assets/logic.js
+++ assets/logic.js
@@ -2,7 +2,6 @@
 var musixCall = fetch(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=cumulonimbus&page_size=3&page=1&s_track_rating=desc&apikey=${apiKEY}`, {
 });
 
-var apiKEY = "740dcd30745d6e3536b315d57ead722b"
 
 console.log(musixCall)
 console.log("Connected to HTML")
