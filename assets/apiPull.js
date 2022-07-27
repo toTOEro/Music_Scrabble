302496befd081465407553e1e80facb3b73ebb28
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-26 19:55:06 -0700

    initial test code apiPull

--- assets/apiPull.js
+++ assets/apiPull.js
@@ -1,2 +1,16 @@
-console.log(process.env.musix_API)
-console.log(process.env.spotify_API)
+
+
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+var client_id = 'e9b231023ef4412d9b11150e6f14f96b'; // Your client id
+var spotify_secret = process.env.spotify_API; // Your secret
+var musix_secret = process.env.musix_API;
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+
+console.log(spotify_secret)
+console.log(musix_secret)
