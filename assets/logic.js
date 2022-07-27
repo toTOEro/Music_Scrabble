ccbe396d03fec38fa593c1a55520d36d911f4b6b
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-26 18:36:38 -0700

    Implemented musixmatch APIcall

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,62 @@
 
 var apiKEY = "740dcd30745d6e3536b315d57ead722b"
 
-var musixCall = fetch(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=cumulonimbus&page_size=3&page=1&s_track_rating=desc&apikey=${apiKEY}`, {
-});
+var lyrics = "if,i,were,a,boy"
+
+var musixUrl = `https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${lyrics}&page_size=10&page=1&s_track_rating=desc&apikey=${apiKEY}`;
+
+var tracks = {};
+var songArray = [];
+var music = [];
 
 
 
-// console.log(musixCall)
-// console.log("Connected to HTML")
+function musixApiCall(url) {
+    const mus = fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.message.header.status_code)
+            if (data.message.header.status_code != 200) {
+                throw new Error('Network response not OK');
+            };
+            music = data.message.body.track_list;
+            console.log(music)
+            for (let i = 0; i < music.length; i++) {
+                var song = music[i].track;
+
+                const track = {
+                    album: song.album_name,
+                    artist: song.artist_name,
+                    song: song.track_name,
+                };
+
+                songArray.push(track);
 
 
-// console.log(process.env.MUSIXMATCH_API)
+            };
+            console.log(songArray);
+            return data;
+        });
+
+    async function processData() {
+
+    };
+    processData();
+}
+
+
+function spotifyApiCall(song) {
+    const spotifySong = fetch(url)
+        .then((response) => response.json())
+        .then()
+
+}
+
+
+
+musixApiCall(musixUrl)
 
 // Establishing API's from Spotify/SoundCloud and MusiX
 
