c755728a3abf989ff51e8b0930e2e773e423e7e3
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-28 19:59:47 -0700

    API Logic completed, starting rendering of results

--- assets/logic.js
+++ assets/logic.js
@@ -1,19 +1,25 @@
-var apiKEY = "740dcd30745d6e3536b315d57ead722b"
 
-var lyrics = "if,i,were,a,boy"
+var lyricsEl = document.getElementById('lyrics');
+var submitBt = document.getElementById('submitBt');
+var lyricEntryEl = document.getElementById('submitBt');
+var searchBt = document.getElementById('searchBt');
 
+var apiKEY = "740dcd30745d6e3536b315d57ead722b";
 
-var musixUrl = `https://cors-anywhere-vercel-chi.vercel.app/api/cors?url=https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${lyrics}&page_size=10&page=1&s_track_rating=desc&apikey=${apiKEY}`
-
+var lyrics = []
 
 var tracks = {};
 var spotifyApiArray = [];
 
 var music = [];
 
+// musixApiCall sends a request to the musixAPi to match the entered words to the lyrics of songs. It returns the top 10
+// songs as a result
+function musixApiCall() {
+    lyricString = lyrics.join(',');
+    var musixUrl = `https://cors-anywhere-vercel-chi.vercel.app/api/cors?url=https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${lyricString}&page_size=10&page=1&s_track_rating=desc&apikey=${apiKEY}`
 
-function musixApiCall(url) {
-    const mus = fetch(url)
+    const mus = fetch(musixUrl)
         .then(function (response) {
             console.log(response);
             return response.json();
@@ -38,7 +44,7 @@ function musixApiCall(url) {
         });
 };
 
-
+// spotifyApiCall takes the song information and sends a request to the Spotify API for song information and the URL to the album art.
 function spotifyApiCall(songs) {
 
     var client_id = "e9b231023ef4412d9b11150e6f14f96b";
@@ -77,41 +83,55 @@ function spotifyApiCall(songs) {
                         return response.json();
                     })
                     .then(function (data) {
+                        console.log(data)
                         var topMatch = data.tracks.items[0];
-                        var spotifySongData = {
-                            song: topMatch.name,
-                            album: topMatch.album.name,
-                            albumArt: topMatch.album.images[0],
-                            artist: topMatch.artists[0].name,
-                            url: topMatch.external_urls.spotify,
-                            preview: topMatch.preview_url,
+                        // If statement to handle situations where there is no song result
+                        if (data.tracks.total == 0) {
+                            i++
+                        } else {
+                            var spotifySongData = {
+                                song: topMatch.name,
+                                album: topMatch.album.name,
+                                albumArt: topMatch.album.images[0],
+                                artist: topMatch.artists[0].name,
+                                url: topMatch.external_urls.spotify,
+                                preview: topMatch.preview_url,
+                            };
+                            songDataArray.push(spotifySongData);
                         };
-                        songDataArray.push(spotifySongData);
+                        renderResults(songDataArray);
                     });
             };
-            console.log(songDataArray)
-
         });
 };
 
-// For loop to iterate over the songArray
+// The logWord function takes the user's entry and pushes it to an array
+function logWord(event) {
+    event.preventDefault();
+    var enteredWord = lyricsEl.value;
 
-// Pull song URLs
+    if (onlyCharacters(enteredWord)) {
+        lyrics.push(enteredWord);
+        enteredWord = "";
+    } else {
+        enteredWord = "";
+    };
+    console.log(lyrics)
+};
 
-// populate an array of objects with title + album + artist URL
-// -> renderSongs()
+// onlyCharacters function to verify only characters were added.
+// https://bobbyhadz.com/blog/javascript-check-if-string-contains-only-letters#:~:text=Use%20the%20test()%20method,only%20letters%20and%20false%20otherwise.
+function onlyCharacters(entry) {
+    return /^[a-zA-z]+$/.test(entry);
+};
 
-// const spotifySong = fetch(url)
-//     .then((response) => response.json())
-//     .then()
+function renderResults(songInformation) {
+    window.localStorage.setItem("spotifyInformation", JSON.stringify(songInformation));
+    window.location.href = "./results.html";
 
-
-
-
-
-musixApiCall(musixUrl)
-
-// Establishing API's from Spotify/SoundCloud and MusiX
+}
 
 
-// Logic to determine song from words
+lyricEntryEl.addEventListener("click", logWord)
+searchBt.addEventListener('click', musixApiCall)
+
